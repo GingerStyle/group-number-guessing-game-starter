c4fbf8221ad99c498c5002a6b75281e2aff78478
Myles Voigt <myles.voigt@gmail.com>
2022-12-05 20:53:05 -0600

    project done forgot to commit

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,5 +1,64 @@
 $(document).ready(handleReady);
 
+let number = 0;
+
 function handleReady() {
   console.log("jquery is loaded!")
+  getRandomNum();
+
+  $('#submit').on('click', getInputs);
+}
+
+function getRandomNum(){
+  $.ajax({
+    method: 'GET',
+    url: '/randomNum',
+  }).then(function(response){
+    number = response;
+    console.log('number =', number);
+  }).catch(function(error){
+    console.log(error);
+    alert(error.responseText);
+  });
+}
+
+function getInputs(){
+  let guesses = {
+    first: $('#firstGuess').val(),
+    second: $('#secondGuess').val(),
+    third: $('#thirdGuess').val(),
+  }
+  $.ajax({
+    method: 'POST',
+    url: '/randomNum',
+    data: guesses,
+  }).then(function(response){
+    
+  }).catch(function(error){
+    console.log(error);
+    alert(error.responseText);
+  });
+  getResults();
+}
+
+function getResults(){
+  $.ajax({
+    method: 'GET',
+    url: '/compare',
+  }).then(function(response){
+    render(response);
+  }).catch(function(error){
+    console.log(error);
+    alert(error.responseText);
+  });
+}
+
+function render(response){
+  for (item of response){
+    $('#result').append(`
+      <li>
+        ${item.name}'s guess was ${item.status}, and their guess was ${item.guess}.
+      </li>
+    `)
+  }
 }
--- server/server.js
+++ server/server.js
@@ -1,7 +1,9 @@
+
+
 const express = require('express');
 const bodyParser = require('body-parser')
 const app = express();
-const PORT = 5000;
+const PORT = 5001;
 
 // This must be added before GET & POST routes.
 app.use(bodyParser.urlencoded({extended:true}))
@@ -15,3 +17,52 @@ app.use(express.static('server/public'));
 app.listen(PORT, () => {
   console.log ('Server is running on port', PORT)
 })
+
+let guesses = {};
+let result = 0;
+
+app.get('/randomNum', function (req, res) {
+  result = Math.round(Math.random() * (25 - 1) + 1);
+  console.log('result =', result);
+  res.send(String(result));
+})
+
+app.post('/randomNum', function (req, res) {
+  console.log("in post request", req.body);
+  guesses = req.body;
+  console.log(guesses);
+  res.sendStatus(201);
+})
+
+app.get('/compare', function(req, res) {
+  let firstGuess = {
+    name: 'First person',
+    status: compare(guesses.first),
+    guess: guesses.first
+  }
+  let secondGuess = {
+    name: 'Second person',
+    status: compare(guesses.second),
+    guess: guesses.second
+  }
+  let thirdGuess = {
+    name: "Third person",
+    status: compare(guesses.third),
+    guess: guesses.third
+  }
+  let guessArray = [];
+  guessArray.push(firstGuess, secondGuess, thirdGuess);
+  res.send(guessArray);
+})
+
+function compare(num){
+if (num < result){
+  return "lesser";
+}
+else if(num > result){
+  return "greater";
+}
+else if(num == result){
+  return "bingo";
+}
+}
